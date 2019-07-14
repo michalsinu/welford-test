ee330daf3c41bb200db1a166b13f68fb79c616a9
michalsinu <michalsinu@gmail.com>
2019-07-14 22:12:21 +0100

    tweaks, css and images minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var cleanCSS 	= require('gulp-clean-css');
+var image 		= require('gulp-image');
 
-// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("src/scss/*.scss")
         .pipe(sass())
@@ -10,7 +11,20 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-// Static Server + watching scss/html files
+gulp.task('minifyCSS', function() {
+	return gulp.src('src/css/*.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('src/min/css'))
+
+	console.log("CSS minified");
+});
+
+gulp.task('minifyIMG', function() {
+	return gulp.src('src/assets/*')
+	    .pipe(image())
+	    .pipe(gulp.dest('src/min/assets'));
+});
+
 gulp.task('serve', gulp.series('sass', function() {
 
     browserSync.init({
@@ -20,4 +34,6 @@ gulp.task('serve', gulp.series('sass', function() {
     gulp.watch("src/scss/*.scss", gulp.series(['sass']));
     gulp.watch("src/*.html").on('change', browserSync.reload);
 }));
+
+gulp.task('default', gulp.series(['minifyCSS'], ['minifyIMG'], ['serve']));
 
